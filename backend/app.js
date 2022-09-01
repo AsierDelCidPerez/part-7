3db3bd25c9e4d2f428ad21d240f3f29fde005ca4
Asier Del Cid Pérez <103944663+AsierDelCidPerez@users.noreply.github.com>
2022-09-01 18:44:47 +0200

    01/09/2022

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,9 @@ require('express-async-errors')
 const logger = require('./utils/logger')
 const userRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
-const {errorHandler, unknownEndpoint, getIpAdrr} = require('./utils/middleware')
+const {errorHandler, unknownEndpoint, getIpAdrr, maintenanceEndpoint, closeEndpoint} = require('./utils/middleware')
 const loginRouter = require('./controllers/login')
+const helmet = require('helmet')
 
 logger.info(`connecting to ${config.PORT}`)
 
@@ -16,7 +17,8 @@ mongoose.connect(config.MONGODB_URI)
         .then(() => logger.info('Connected to MONGODB'))
         .catch(err => logger.error(`Error connecting to MongoDB: ${err.message}`))
 
-app.use(getIpAdrr)
+app.use(helmet())
+// app.use(closeEndpoint)
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors()) // Permite conexiones cruzadas, es decir de diferentes dominios
@@ -31,5 +33,6 @@ app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
 app.use(errorHandler)
+app.use(unknownEndpoint)
 
 module.exports = app
--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,21 +1,72 @@
 const morgan = require('morgan')
 
-const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
-    if(error.name === 'CastError') res.status(400).send({error: 'malformated id'})
-    else if(error.name === 'ValidationError') res.status(400).json({error: error.message})
-    else if(error.name === 'JsonWebTokenError') res.status(401).json({error: 'Invalid token'})
-    next(error)
+const getAgent = req => {
+    return {
+        agent: req.get('User-Agent'),
+        ip: req.ip
+    }
 }
 
-const getIpAdrr = (req, _, next) => {
-    const ip = morgan(':remote-addr')
-    req.ip = ip
-    next()
+const errorHandler = (error, req, res, next) => {
+    const myError = {
+        errorCode: 400,
+        time: new Date(),
+        error: {
+            name: error.name,
+            message: error.message,
+            requestedUrl: req.path
+        },
+        agent: getAgent(req)
+    }
+    res.status(400).send(myError)
+}
+
+const maintenanceEndpoint = (req, res, next) => {
+    const error = {
+        errorCode: 503,
+        time: new Date(),
+        errorDetails: {
+            description: 'Service is under maintenance',
+            requestedUrl: req.path
+        },
+        agent: getAgent(req)
+    }
+    res.status(503).send(error)
+}
+
+const closeEndpoint = (req, res, next) => {
+    const error = {
+        errorCode: 503,
+        time: new Date(),
+        errorDetails: {
+            description: 'Service Closed',
+            requestedUrl: req.path
+        },
+        agent: getAgent(req)
+    }
+    res.status(503).send(error)
 }
 
 const unknownEndpoint = (req, res, next) => {
-    res.status(404).send({error: 'Not found'})
+    const error = {
+        errorCode: 404,
+        time: new Date(),
+        errorDetails: {
+            description: 'Not found',
+            requestedUrl: req.path
+        },
+        helping: {
+            causes: [
+                'URL hard-writed',
+                'This page was deleted'
+            ],
+            possibleSolutions: [
+                'Review the URL written'
+            ]
+        },
+        agent: getAgent(req)
+    }
+    res.status(404).send(error)
 }
 
-module.exports = {errorHandler, unknownEndpoint, getIpAdrr}
+module.exports = {errorHandler, unknownEndpoint, maintenanceEndpoint, closeEndpoint}
